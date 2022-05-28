d745d0e0c069d4f38f476d7a4d7750694d39026c
양동해 <xgf6580@gmail.com>
2022-05-28 16:41:25 +0000

    revise 404 page (only news-post)

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
 
 @app.route("/test/method", methods=["GET", "POST"])
 def method():
     if request.method == "GET": return "GET으로 전달"
     elif request.method == "POST": return "POST로 전달"
 
-
-def page_not_found(e):
-    return render_template("404.html"), 404
     
 if __name__ == "__main__":
-    app.register_error_handler(404, page_not_found)
     app.run(host='0.0.0.0', port=5000, debug=True)
