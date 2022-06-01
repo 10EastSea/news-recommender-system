4f241d432e7c9f273c8b39d2f92347de59400424
양동해 <xgf6580@gmail.com>
2022-06-01 13:49:27 +0000

    mobile

--- static/js/index_ready.js
+++ static/js/index_ready.js
@@ -1,4 +1,21 @@
 /* index.html document ready */
 $(document).ready(function () {
-    console.log("News Recommender System");
+    // mobile
+    $(window).resize(function() {
+        var width_size = window.outerWidth;
+        
+        if(width_size < 768) {
+            $("#main_impression_img_layout").removeClass("col-8");
+            $("#main_impression_list_layout").removeClass("col-4");
+            
+            $("#main_impression_img_layout").addClass("col");
+            $("#main_impression_list_layout").addClass("col mt-1");
+        } else {
+            $("#main_impression_img_layout").removeClass("col");
+            $("#main_impression_list_layout").removeClass("col mt-1");
+            
+            $("#main_impression_img_layout").addClass("col-8");
+            $("#main_impression_list_layout").addClass("col-4");
+        }
+    });
 });
