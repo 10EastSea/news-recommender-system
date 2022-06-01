cc18fdb496e72a18733d290200f42f496a5a9c4b
양동해 <xgf6580@gmail.com>
2022-06-01 14:00:39 +0000

    revise mobileView()

--- static/js/index_ready.js
+++ static/js/index_ready.js
@@ -1,9 +1,22 @@
 /* index.html document ready */
-$(document).ready(function () {
-    // mobile
+function mobileView() {
+    var width_size = window.outerWidth;    
+    if(width_size < 768) {
+        $("#main_impression_img_layout").removeClass("col-8");
+        $("#main_impression_list_layout").removeClass("col-4");
+
+        $("#main_impression_img_layout").addClass("col");
+        $("#main_impression_list_layout").addClass("col mt-1");
+    } else {
+        $("#main_impression_img_layout").removeClass("col");
+        $("#main_impression_list_layout").removeClass("col mt-1");
+
+        $("#main_impression_img_layout").addClass("col-8");
+        $("#main_impression_list_layout").addClass("col-4");
+    }
+    
     $(window).resize(function() {
-        var width_size = window.outerWidth;
-        
+        width_size = window.outerWidth;
         if(width_size < 768) {
             $("#main_impression_img_layout").removeClass("col-8");
             $("#main_impression_list_layout").removeClass("col-4");
@@ -18,4 +31,8 @@ $(document).ready(function () {
             $("#main_impression_list_layout").addClass("col-4");
         }
     });
+}
+
+$(document).ready(function () {
+    mobileView();
 });
